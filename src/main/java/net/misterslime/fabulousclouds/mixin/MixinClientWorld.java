c22eacdf86916df09b7bea0a7a3645de0a7b7281
misterslime <30942693+misterslime@users.noreply.github.com>
2021-08-05 13:18:27 -0600

    Small Refactor

--- src/main/java/net/misterslime/fabulousclouds/mixin/MixinClientWorld.java
+++ src/main/java/net/misterslime/fabulousclouds/mixin/MixinClientWorld.java
@@ -1,8 +1,6 @@
 package net.misterslime.fabulousclouds.mixin;
 
 import com.mojang.blaze3d.systems.RenderSystem;
-import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.render.CameraSubmersionType;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.util.math.Vec3d;
 import net.misterslime.fabulousclouds.FabulousClouds;
@@ -16,18 +14,18 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 public class MixinClientWorld {
 
     @Inject(method = "getCloudsColor(F)Lnet/minecraft/util/math/Vec3d;", at = @At("RETURN"), cancellable = true)
-    private void setCloudColor(CallbackInfoReturnable<Vec3d> cir) {
-        Vec3d cloudColor = cir.getReturnValue();
-        CameraSubmersionType cameraSubmersionType = MinecraftClient.getInstance().gameRenderer.getCamera().getSubmersionType();
+    private void setCloudColor(float tickDelta, CallbackInfoReturnable<Vec3d> cir) {
         FabulousCloudsConfig config = FabulousClouds.getConfig();
 
-        if (cameraSubmersionType == CameraSubmersionType.NONE && config.vibrant_clouds) {
+        if (config.vibrant_clouds) {
+            Vec3d cloudColor = cir.getReturnValue();
             float[] fogColor = RenderSystem.getShaderFogColor();
+            float vibranceIntensity = config.vibrance_intensity;
 
             double[] tint = new double[3];
-            tint[0] = ((fogColor[0] * config.vibrance_intensity) + cloudColor.x) / 2;
-            tint[1] = ((fogColor[1] * config.vibrance_intensity) + cloudColor.y) / 2;
-            tint[2] = ((fogColor[2] * config.vibrance_intensity) + cloudColor.z) / 2;
+            tint[0] = ((fogColor[0] * vibranceIntensity) + cloudColor.x) / 2;
+            tint[1] = ((fogColor[1] * vibranceIntensity) + cloudColor.y) / 2;
+            tint[2] = ((fogColor[2] * vibranceIntensity) + cloudColor.z) / 2;
 
             cir.setReturnValue(new Vec3d(tint[0], tint[1], tint[2]));
         }
